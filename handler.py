bd846e5c0a09c504435cca0ac394660a6328bbfb
nstreling <nstreling@telenor.hu>
2022-11-10 09:11:17 +0100

    delete comments from handler

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def ingcom_submit(event, context):
 
-    # ensure list of search parameters (states, makes) are present in request
+    # ensure list of search parameters (city, property type, deal type) are present in request
     if 'city_unic' not in event or 'property_type' not in event or 'deal' not in event:
         PROPERTY_TYPE='+'
         DEAL='+'
@@ -47,7 +47,7 @@ def ingcom_submit(event, context):
     cityCsvName = 'ingcom_db/ingcom_cities.csv'
     city_obj = s3.get_object(Bucket=bucketName, Key=cityCsvName)
     df = pd.read_csv(city_obj['Body'])
-    # df=pd.read_csv("/mnt/A42A8BE22A8BB03A/Python/AWS_IC_Scraper/ingcom_scraper/db/cities/ingcom_cities.csv")
+
     searches = df[df['city_unic'].isin(CITY)]
     
     # placeholder for jobs to be submitted
@@ -79,8 +79,6 @@ def ingcom_submit(event, context):
                 _job['jobName'] = f'{property_type}-{_city}-{deal}'
                 job_queue.append(_job)
     
-    # for i in job_queue:
-    #     print(i.items())
 
     for j in job_queue:
         res = batch.submit_job(**j)
